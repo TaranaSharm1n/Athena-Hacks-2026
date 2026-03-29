08dd9c3ded5aa4709d7cfcb64528cc596736ba9c
Lynleaf <evychan@usc.edu>
2026-03-29 12:22:22 -0700

    timer and session page fix

--- app/session/page.tsx
+++ app/session/page.tsx
@@ -3,13 +3,19 @@ import WebcamFeed from "../components/webcam";
 
 export default function Session() {
   return (
-    <main className="relative h-screen w-screen flex justify-center items-center bg-gray-100">
+    <main className="h-screen w-screen flex justify-center"
+    style={{
+        background: "#dce8f5",
+        backgroundImage: "repeating-linear-gradient(0deg, rgba(180,210,240,0.5) 0px, rgba(180,210,240,0.5) 1px, transparent 1px, transparent 40px)",
+      }}
+    >
+      <img src="/assets/Table.png" alt="table" className="bottom-0" />
+      
       
       <div className="absolute top-30 right-16 rounded-full">
         <WebcamFeed />
       </div>
 
-      
       <div className="absolute top-80 right-80 text-center">
         <Timer />
       </div>
