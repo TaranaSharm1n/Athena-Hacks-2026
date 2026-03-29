cca717c5a84d58f2a5d46c647336b58bbcef63ac
Tarana Sharmin <144616498+TaranaSharm1n@users.noreply.github.com>
2026-03-28 19:16:55 -0700

    buttons

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,21 @@
 import Image from "next/image";
-import link from "next/link";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main>
       <img src="/logo.png" alt="Athena Hacks Logo" />
       
-      <div>
-        <link href="app/session">
-          <button>Start Session</button>
-        </link>
+      <div className="centered">
+        <button className="bg-red-950 hover:bg-amber-900 text-white font-bold py-2 px-4 rounded">
+          Start Session
+        </button>
+        <br /> <br />
+        <button className="bg-red-950 hover:bg-amber-900 text-white font-bold py-2 px-4 rounded">
+          My Garden
+        </button>
       </div>
 
-      <div>
-        <link href="app/garden">
-          <button>My Garden</button>
-        </link>
-      </div>
     </main>
   )
 }
