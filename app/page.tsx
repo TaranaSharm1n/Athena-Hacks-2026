33526c69e2fecca7dd50007cbecc37553fb99047
Tarana Sharmin <144616498+TaranaSharm1n@users.noreply.github.com>
2026-03-28 19:36:09 -0700

    layout fixed

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,18 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main>
-      <img src="/logo.png" alt="Athena Hacks Logo" />
+      {/* <div className="flex flex-col items-center justify-center h-screen gap-0.125">
+       
+      </div>
+       */}
       
-      <div className="centered">
+
+      <div className="flex flex-col items-center justify-center h-screen gap-0.125">
+        <img className = "h-16 w-16" src="/assets/flowerPlaceholder.png" alt="Athena Hacks Logo" />
+        <h1 className="text-4xl font-bold text-center">Welcome to Athena Hacks!</h1>
+        <p className="text-lg text-center">Grow Your Producitivity!</p>
+        <br /> <br />
+
         <button className="bg-red-950 hover:bg-amber-900 text-white font-bold py-2 px-4 rounded">
           Start Session
         </button>
