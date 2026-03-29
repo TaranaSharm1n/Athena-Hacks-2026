310043a76a27e5c6dfea1775cc0c7d8d830f8ad1
Tarana Sharmin <144616498+TaranaSharm1n@users.noreply.github.com>
2026-03-28 18:28:35 -0700

    updated main page for functionability of buttons

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,28 @@
 import Image from "next/image";
+import link from "next/link";
 
 export default function Home() {
   return (
-    <main className="bg-green-100 min-h-screen flex items-center justify-center">
-      <h1 className="text-4xl font-bold text-green-800">Growing your productivity 🌱</h1>
+    <main>
+      <img src="/logo.png" alt="Athena Hacks Logo" />
+      
+      <div>
+        <link href href="/session" passHref>
+          <button>Start Session</button>
+        </link>
+      </div>
+
+      <div>
+        <link href href="/garden" passHref>
+          <button>My Garden</button>
+        </link>
+      </div>
     </main>
-  );
+  )
 }
+
+
+
+
 
 
---
+++ app/session/page.tsx
@@ -0,0 +1,7 @@
+export default function Session() {
+  return (
+    <main>
+      <h1>Active Session</h1>
+    </main>
+  )
+}
