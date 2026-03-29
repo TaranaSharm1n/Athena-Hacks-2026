d87ee1126cd8073ede9a247ae64b6dc4592846b3
Tarana Sharmin <144616498+TaranaSharm1n@users.noreply.github.com>
2026-03-28 18:14:13 -0700

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="bg-green-100 min-h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-green-800">Growing your productivity 🌱</h1>
+    </main>
+  );
+}
+
+
